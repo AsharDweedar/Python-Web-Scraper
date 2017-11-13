a96bccba69771086e31a12e4141eb1deb699c575
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-11-13 10:13:26 +0200

    i recieve a post request

--- logIn/urls.py
+++ logIn/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'', views.index, name='index'),
-    url(r'/post', views.submit, name='index'),
+    url(r'^post/', views.submit, name='submit'),
 ]
 

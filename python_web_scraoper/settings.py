3096c3d38437d70bc937fad75fa7e25d12bd109c
AsharDweedar <Ashar.Dweedar@gmail.com>
2018-09-14 17:53:09 +0300

    changes i don't know about

--- python_web_scraoper/settings.py
+++ python_web_scraoper/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'python_web_scraoper.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-''' 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -86,15 +86,15 @@ DATABASES = {
         'PORT': '3306',
     }
 }
- '''
- 
+
+''' 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
- 
+'''
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
